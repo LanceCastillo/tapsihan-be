e0cb989b9c89184effd9f4bb5a58c9c170e5c821
Lance Castillo <castilloxlance@gmail.com>
2024-07-19 03:09:34 +0800

    Remove makePayment

--- src/router/payment.ts
+++ src/router/payment.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 
-import { buySingleProduct, makePayment } from '../controllers/payment';
+import { buySingleProduct} from '../controllers/payment';
 
 export default (router:express.Router) =>{
-    router.post('/payment', makePayment)
     router.post('/buy-now', buySingleProduct);
 
 };
